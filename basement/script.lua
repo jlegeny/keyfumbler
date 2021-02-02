35f94058f3e4a0b4c0396f281bb98957da786389
Jozef Legeny <jozef@legeny.fr>
2021-02-02 20:55:25 +0100

    Lv: Update the level

--- basement/script.lua
+++ basement/script.lua
@@ -2,15 +2,52 @@ local util = require 'util'
 local Map = require 'map'
 local Key = require 'object/key'
 
-local items = {
+local keys = {
+  tunnel_key = Key.random(Key.Material.STEEL),
   entrance_key = Key.random(Key.Material.BRASS),
+  cellar_key = Key.random(Key.Material.COPPER),
+  fake_key_1 = Key.random(),
+  fake_key_2 = Key.random(),
 }
+
+local wrong_key_ramble = {
+  "Ugh! It doesn't fit!",
+  "Wrong key.",
+  "Hmm, do I have the right key?",
+  "Nope.",
+  "I would have sworn this was the right one.",
+  "Unf.",
+  "Maybe it's bent?",
+}
+
+local wkridx = 0
+
+local key_not_turn_ramble = {
+  "It fits, but doesn't turn. Must be wrong.",
+  "If I turn it any more I might break it.",
+}
+
+local kntidx = 0
 
 local locks = {
   ['entrance.door'] = {
-    key_type = items.entrance_key.key_type,
-    wording = items.entrance_key.wording,
-    biting = items.entrance_key.biting,
+    key_type = keys.entrance_key.key_type,
+    wording = keys.entrance_key.wording,
+    biting = keys.entrance_key.biting,
+    locked = true,
+    inserted_key = nil,
+  },
+  ['cellar.door'] = {
+    key_type = keys.cellar_key.key_type,
+    wording = keys.cellar_key.wording,
+    biting = keys.cellar_key.biting,
+    locked = true,
+    inserted_key = nil,
+  },
+  ['tunnel.door'] = {
+    key_type = keys.tunnel_key.key_type,
+    wording = keys.tunnel_key.wording,
+    biting = keys.tunnel_key.biting,
     locked = true,
     inserted_key = nil,
   }
@@ -30,13 +67,15 @@ function init(game)
     color = col_myself,
   }
   game.dialogue = nil
-  Key.render(items.entrance_key)
+  for _, key in pairs(keys) do
+    Key.render(key)
+  end
 
 
   -- cheats
   -- entrance key
-  -- game.player.inventory[622] = items.entrance_key
-  locks['entrance.door'].locked = false
+  -- game.player.inventory[622] = keys.entrance_key
+  -- locks['entrance.door'].locked = false
   game:update_inventory()
 end
 
@@ -64,35 +103,98 @@ function dialogue(id)
     }
   elseif id == 'intro.04' then
     game.dialogue = nil
-  elseif id == 'ramble.keyinlock' then
+  elseif id == 'fireplace.1' then
+    game.dialogue = {
+      id = 'fireplace.2',
+      image = nil,
+      text = "It's a bit low, but if I [C]rouch I can fit.",
+      color = col_myself,
+    }
+  elseif id == 'fireplace.2' then
+     game.dialogue = nil
+  elseif id == 'ramble' then
     game.dialogue = nil
+  end
+end
+
+function near_door(alias, game)
+  if locks[alias].inserted_key then
+    if locks[alias].locked then
+      game.overlay_text = 'Press [E] to turn the key or [Q] to remove it.'
+    else
+      game.overlay_text = 'Unlocked. Press [Q] to remove the key.'
+    end
+  elseif locks[alias].locked then
+    if game.volatile.key_count > 0 then
+      if game.keyring.state == 'closed' then
+        game.overlay_text = 'Press [F] to whip out the key.'
+      elseif game.keyring.state == 'open' then
+        game.overlay_text = 'Press [E] to insert the key.'
+      end
+    else
+      game.overlay_text = 'The door is locked and you have no keys.'
+    end
+  else
+    game.overlay_text = 'Press [E] to open or close the door.'
+  end
+end
+
+function trigger_door(alias, game)
+  if locks[alias].inserted_key then
+    if locks[alias].locked then
+      if Key.turns_in(locks[alias]) then
+        locks[alias].locked = false
+      else
+        game.dialogue = {
+          id = 'ramble',
+          image = nil,
+          text = key_not_turn_ramble[kntidx + 1],
+          color = col_myself,
+        }
+        kntidx = (kntidx + 1) % #key_not_turn_ramble
+      end
+    end
+  elseif not locks[alias].locked then
+    local door_id = game.map.volatile.raliases[alias]
+    toggle_door(door_id, game)
+  elseif game.keyring.state == 'open' and game:chosen_key() then
+    local ck = game:chosen_key()
+    if Key.fits(ck, locks[alias]) then
+      locks[alias].inserted_key = game:chosen_key()
+    else
+      game.dialogue = {
+        id = 'ramble',
+        image = nil,
+        text = wrong_key_ramble[wkridx + 1],
+        color = col_myself,
+      }
+      wkridx = (wkridx + 1) % #wrong_key_ramble
+    end
   end
 end
 
 function near(id, game)
   local alias = game.map.aliases[id]
-  if alias == '_entrance.key' and not flags['has.entrance.key'] then
+  if alias == '_tunnel.key' and not flags['has.tunnel.key'] then
     game.overlay_text = 'Press [E] to pick up the key.'
+  elseif alias == '_entrance.key' and not flags['has.entrance.key'] then
+    game.overlay_text = 'Press [E] to pick up the key.'
+  elseif alias == '_cellar.keys' and not flags['has.cellar.keys'] then
+    game.overlay_text = 'Press [E] to pick up the keys off the wall.'
   elseif alias == '_entrance.door' then
-    if locks['entrance.door'].inserted_key then
-      if locks['entrance.door'].locked then
-        game.overlay_text = 'Press [E] to turn the key or [Q] to remove it.'
-      else
-        game.overlay_text = 'Unlocked. Press [Q] to remove the key.'
-      end
-    elseif locks['entrance.door'].locked then
-      if game.player:has(game.map, 'entrance.key') then
-        if game.keyring.state == 'closed' then
-          game.overlay_text = 'Press [F] to whip out the key.'
-        elseif game.keyring.state == 'open' then
-          game.overlay_text = 'Press [E] to insert the key.'
-        end
-      else
-        game.overlay_text = 'The door is locked and you have no keys.'
-      end
-    else
-      game.overlay_text = 'Press [E] to open or close the door.'
+    near_door('entrance.door', game)
+  elseif alias == '_entrance.fireplace' then
+    if not flags['commented.on.fireplace'] then
+      game.dialogue = {
+        id = 'fireplace.1',
+        image = nil,
+        text = "There was originally a fireplace here. But we remade it into a playroom for kids.",
+        color = col_myself,
+      }
+      flags['commented.on.fireplace'] = true
     end
+  elseif alias == '_cellar.door' then
+    near_door('cellar.door', game)
   end
 end
 
@@ -125,28 +227,24 @@ function trigger(id, trigger, game)
   local alias = game.map.aliases[id]
 
   print('triggered', id, alias)
-  if alias == '_entrance.key' then
+  if alias == '_tunnel.key' then
+    id, _ = game.map:pick_up('tunnel.key')
+    game.player.inventory[id] = keys.tunnel_key
+    game:update_inventory()
+    flags['has.tunnel.key'] = true
+  elseif alias == '_entrance.key' then
     id, _ = game.map:pick_up('entrance.key')
-    print('picked up ' .. id)
-    game.player.inventory[id] = items.entrance_key
+    game.player.inventory[id] = keys.entrance_key
     game:update_inventory()
     flags['has.entrance.key'] = true
+  elseif alias == '_cellar.keys' then
+    game.player.inventory[Map.get_id(game.map)] = keys.fake_key_1
+    game.player.inventory[Map.get_id(game.map)] = keys.cellar_key
+    game.player.inventory[Map.get_id(game.map)] = keys.fake_key_2
+    game:update_inventory()
+    flags['has.cellar.keys'] = true
   elseif alias == '_entrance.door' then
-    if locks['entrance.door'].inserted_key then
-      if locks['entrance.door'].locked then
-        if Key.turns_in(locks['entrance.door']) then
-          locks['entrance.door'].locked = false
-        end
-      end
-    elseif not locks['entrance.door'].locked then
-      local door_id = game.map.volatile.raliases['entrance.door']
-      toggle_door(door_id, game)
-    elseif game.keyring.state == 'open' and game:chosen_key() then
-      local ck = game:chosen_key()
-      if Key.fits(ck, locks['entrance.door']) then
-        locks['entrance.door'].inserted_key = game:chosen_key()
-      end
-    end
+    trigger_door('entrance.door', game)
   end
   if id == 621 then
     local door = game.map.splits[608]
